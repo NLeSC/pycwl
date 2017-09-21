19e45a3e3429690fa1f2bb4abc84b2fbfa0bed3b
Ronald van Haren <r.vanharen@esciencecenter.nl>
2017-09-21 16:12:32 +0200

    1st runner

--- pycwl/runner.py
+++ pycwl/runner.py
@@ -1,7 +1,16 @@
 import logging
 
+from pycwl.parser import Parser
 
-class runner:
+logger = logging.getLogger(__name__)
+
+
+class Runner:
     def __init__(self, args):
-        logger = logging.getLogger(__name__)
-        logging.info('runner class')
+        logger.info('runner class')
+        workflow = Parser.parse_cwlstream(args.workflow)
+        if args.job_object:
+            job_object = Parser.parse_job_object(args.job_object)
+        else:
+            # empty job_object
+            job_object = JobObject()
